fd6c2e98202224f3cb097ca235fd842d4c54be2c
DongWook Kim <woodi@overdare.com>
2024-05-08 11:13:32 +0900

    feat: complete omok game

--- client/P2POmokClient.go
+++ client/P2POmokClient.go
@@ -22,7 +22,28 @@ const maxNameLen = 64
 const SERVER_IP = "127.0.0.1"
 const SERVER_PORT = "30000"
 
+var timer *time.Timer
+
 const turnLimit = time.Second * 10
+
+var gamePlayingStatus = PLAYING
+
+type gameStatus int
+type command int
+
+const (
+	MOVE command = iota
+	EXIT
+	GG
+	CHAT
+)
+
+const (
+	PLAYING gameStatus = iota
+	WIN
+	LOSE
+	DRAW
+)
 
 type Board [][]int
 
@@ -30,7 +51,7 @@ type User struct {
 	nickname string
 	ip       string
 	port     int
-	isFirst bool
+	isFirst  bool
 }
 
 func convertToInt(s string) int {
@@ -40,7 +61,6 @@ func convertToInt(s string) int {
 	}
 	return i
 }
-
 
 // ========= Network =========
 
@@ -84,12 +104,12 @@ func match(nickname string) (me User, opponent User) {
 			nickname: nickname,
 			ip:       SERVER_IP,
 			port:     clientPort,
-			isFirst: myTurn == 0,
+			isFirst:  myTurn == 0,
 		}, User{
 			nickname: splitted[0],
 			ip:       splitted[1],
 			port:     convertToInt(splitted[2]),
-			isFirst: opponentTurn == 0,
+			isFirst:  opponentTurn == 0,
 		}
 }
 
@@ -252,6 +272,47 @@ func checkUserName(name string) bool {
 	return true
 }
 
+func runNextTurn(turn *int, count *int, win *int, board Board, x, y int) {
+	board[x][y] = *turn%2 + 1
+	*count++
+	printBoard(board)
+
+	if *count == Row*Col {
+		fmt.Println("draw")
+		gamePlayingStatus = DRAW
+		return
+	}
+
+	*win = checkWin(board, x, y)
+	if *win != 0 {
+		if *win == 1 {
+			fmt.Println("you win")
+			gamePlayingStatus = WIN
+		} else {
+			fmt.Println("you lose")
+			gamePlayingStatus = LOSE
+		}
+		return
+	}
+
+	*turn++
+	timer = time.AfterFunc(turnLimit, func() {
+		fmt.Println("time over")
+		gamePlayingStatus = LOSE
+	})
+
+	clear()
+}
+
+func runTimer(conn *net.UDPConn) {
+	timer = time.AfterFunc(turnLimit, func() {
+		gamePlayingStatus = LOSE
+		fmt.Println("time over")
+		sendMessage(conn, fmt.Sprintf("%d", int(GG)))
+		gamePlayingStatus = LOSE
+	})
+}
+
 func main() {
 	// @STEP 1: get command
 	// example: go run P2POmokClient.go <nickname>
@@ -284,18 +345,22 @@ func main() {
 	}
 	defer sendConn.Close()
 
-	// recv message
-	go func(c *net.UDPConn) {
-		for {
-			msg := recvMessage(c)
-			fmt.Println("recv message:", msg)
-		}
-	}(recvConn)
+	if me.isFirst {
+		fmt.Println("you are first")
+	} else {
+		fmt.Println("you are second")
+	}
 
-	// game logic
-	clear()
+	time.Sleep(1 * time.Second)
+
+	// set timer
+	if me.isFirst {
+		go runTimer(sendConn)
+	}
+
+	// init board
 	board := Board{}
-	// x, y, turn, count, win := -1, -1, 0, 0, 0
+	turn, count, win := 0, 0, 0
 	for i := 0; i < Row; i++ {
 		var tempRow []int
 		for j := 0; j < Col; j++ {
@@ -303,6 +368,44 @@ func main() {
 		}
 		board = append(board, tempRow)
 	}
+
+	// recv message
+	go func(c *net.UDPConn) {
+		for {
+			// message format: <command> ...etc
+			msg := recvMessage(c)
+
+			splitted := strings.Split(strings.TrimSpace(msg), " ")
+			if len(splitted) == 0 {
+				continue
+			}
+
+			cmd, _ := strconv.Atoi(splitted[0])
+
+			switch command(cmd) {
+			case MOVE:
+				// run game logic
+				x := convertToInt(splitted[1])
+				y := convertToInt(splitted[2])
+
+				runNextTurn(&me.port, &me.port, &me.port, board, x, y)
+			case EXIT:
+				fmt.Println("opponent exited")
+				gamePlayingStatus = WIN
+				return
+			case GG:
+				fmt.Println("opponent gave up")
+				gamePlayingStatus = WIN
+				return
+			case CHAT:
+				fmt.Printf("%s> %s\n", opponent.nickname, strings.Join(splitted[1:], " "))
+			}
+
+		}
+	}(recvConn)
+
+	// game logic
+	clear()
 
 	printBoard(board)
 	reader := bufio.NewReader(os.Stdin)
@@ -315,58 +418,76 @@ func main() {
 			return
 		}
 
-		splittedInput := strings.Split(input, " ")
+		splittedInput := strings.Split(strings.TrimSpace(input), " ")
 		if len(splittedInput) == 0 {
 			fmt.Println("invalid command")
 			continue
 		}
 
 		// command list (1): "\gg", "\exit", "\\ <x> <y>"
-		command := splittedInput[0]
-		if strings.HasPrefix(command, "\\") {
-			// @TODO: check valise command
-		} else {
-			// think it as chat message and send it to opponent
-			sendMessage(sendConn, input)
+		cmd := strings.TrimSpace(splittedInput[0])
+
+		if cmd == "\\exit" {
+			sendMessage(sendConn, fmt.Sprintf("%d", int(EXIT)))
+			if timer != nil {
+				timer.Stop()
+			}
+			return
 		}
 
-		// cnt, _ := fmt.Scanf("%d %d ", &x, &y)
+		if gamePlayingStatus != PLAYING {
+			fmt.Println("game is already finished")
+			continue
+		}
 
-		// if cnt != 2 {
-		// 	fmt.Println("error, must enter x y!")
-		// 	time.Sleep(1 * time.Second)
-		// 	continue
-		// } else if x < 0 || y < 0 || x >= Row || y >= Col {
-		// 	fmt.Println("error, out of bound!")
-		// 	time.Sleep(1 * time.Second)
-		// 	continue
-		// } else if board[x][y] != 0 {
-		// 	fmt.Println("error, already used!")
-		// 	time.Sleep(1 * time.Second)
-		// 	continue
-		// }
+		switch cmd {
+		case "\\gg":
+			sendMessage(sendConn, fmt.Sprintf("%d", int(GG)))
+			gamePlayingStatus = LOSE
 
-		// if turn == 0 {
-		// 	board[x][y] = 1
-		// } else {
-		// 	board[x][y] = 2
-		// }
+			if timer != nil {
+				timer.Stop()
+			}
+			continue
+		case "\\\\":
+			// move command
+			if len(splittedInput) != 3 {
+				fmt.Println("invalid command")
+				continue
+			}
 
-		// clear()
-		// printBoard(board)
+			if (opponent.isFirst && turn%2 == 0) || (!opponent.isFirst && turn%2 == 1) {
+				fmt.Println("not your turn")
+				continue
+			}
 
-		// win = checkWin(board, x, y)
-		// if win != 0 {
-		// 	fmt.Printf("player %d wins!\n", win)
-		// 	break
-		// }
+			x := convertToInt(splittedInput[1])
+			y := convertToInt(splittedInput[2])
 
-		// count += 1
-		// if count == Row*Col {
-		// 	fmt.Printf("draw!\n")
-		// 	break
-		// }
+			// check valid move
+			if x < 0 || y < 0 || x >= Row || y >= Col {
+				fmt.Println("error, out of bound!")
+				time.Sleep(1 * time.Second)
+				continue
+			} else if board[x][y] != 0 {
+				fmt.Println("error, already used!")
+				time.Sleep(1 * time.Second)
+				continue
+			}
 
-		// turn = (turn + 1) % 2
+			// stop timer
+			if timer != nil {
+				timer.Stop()
+			}
+
+			// send move command
+			sendMessage(sendConn, fmt.Sprintf("%d %d %d", int(MOVE), x, y))
+
+			runNextTurn(&turn, &count, &win, board, x, y)
+
+		default:
+			// chat command
+			sendMessage(sendConn, fmt.Sprintf("%d %s", int(CHAT), input))
+		}
 	}
 }
